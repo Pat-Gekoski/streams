b6bbc4d07b91bade4be3c65322f1092fea17dc31
Patric Gekoski <PatricGekoski@protonmail.com>
2022-03-16 22:06:51 -0400

    finished streamReducer

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { signIn, signOut } from '../actions/index'
+
+class GoogleAuth extends Component {
+
+   componentDidMount() {
+      window.gapi.load('client:auth2', () => {
+         window.gapi.client
+            .init({
+               clientId: '353815490865-dgbh1g63ogd45rg8tmcjg9f999oorcfl.apps.googleusercontent.com',
+               scope: 'email',
+            })
+            .then(() => {
+               this.auth = window.gapi.auth2.getAuthInstance()
+               this.onAuthChanged(this.auth.isSignedIn.get())
+               this.auth.isSignedIn.listen(this.onAuthChanged)
+            })
+      })
+   }
+
+   onAuthChanged = (isSignedIn) => {
+      if (isSignedIn) {
+         this.props.signIn()
+      } else {
+         this.props.signOut()
+      }
+   }
+
+   onSignInClicked = () => {
+      this.auth.signIn()
+   }
+
+   onSignOutClicked = () => {
+      this.auth.signOut()
+   }
+
+   renderAuthButton() {
+      if (this.props.isSignedIn === null) {
+         return null
+      } else if (this.props.isSignedIn) {
+         return (
+            <button onClick={this.onSignOutClicked} className='ui red google button'>
+               <i className='google icon'></i>
+               Sign Out
+            </button>
+         )
+      } else {
+         return (
+            <button onClick={this.onSignInClicked} className='ui red google button'>
+               <i className='google icon'></i>
+               Sign In With Google
+            </button>
+         )
+      }
+   }
+
+   render() {
+      return <div>{this.renderAuthButton()}</div>
+   }
+}
+
+const mapStateToProps = (state) => {
+   return { isSignedIn: state.auth.isSignedIn }
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth)
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const StreamList = () => {
-	console.log("in streams list")
   return <div>StreamList</div>;
 };
 
